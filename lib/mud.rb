bf106d92a4d60dbb98c22a46b8f72f6dafda34bb
Mike Cartmell <mikec@porridge.(none)>
2012-11-29 20:50:37 +0000

    testing

--- lib/mud.rb
+++ lib/mud.rb
@@ -1,13 +1,24 @@
 require 'sinatra/base'
 require 'sinatra/synchrony'
+require 'redis'
+require 'redis/connection/synchrony'
+
 class Mud < Sinatra::Base
   register Sinatra::Synchrony
+  attr_reader :redis_url
+
+  def initialize
+    @redis_url = ENV["REDISTOGO_URL"]
+    super
+  end
+
+
   get '/' do
     EM::Synchrony.sleep(10)
     "Hello world!"
   end
   
   get '/foo' do
-    "Not blockah"
+    redis_url
   end
 end
