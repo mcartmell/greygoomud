14255bfbd4971f33601acbd7953948c3a0af922a
Mike Cartmell <mike@mikec.me>
2012-12-03 13:23:12 +0000

    more documentation

--- lib/arml.rb
+++ lib/arml.rb
@@ -8,6 +8,10 @@ class Arml
 	class Id
 		attr_reader :key, :id
 
+# Compares to another id by comparing the unique mongo id
+#
+# @param [Arml::Id] other_id The id to compare with
+# @return [Bool] True if equal
 		def ==(other_id)
 			return self.id == other_id.id
 		end
@@ -20,19 +24,28 @@ class Arml
 			@id = id
 		end
 
+# Converts an id to a link
 		def to_href
 			return "#{ENV['ARML_URI_PREFIX']}/#{key}/#{to_s}"
 		end
 
+# Converts to a string
 		def to_s
 			return "#{key}-#{id}"
 		end
 
+# Creates an Id object from a string
+#
+# @param [String] str The string to coerce
+# @return [Arml::Id] The coerced object
 		def self.from_string(str)
 			key,id = str.split(/-/)	
 			return self.new(key,BSON::ObjectId.from_string(id))
 		end
 
+# Serializes to a hash
+#
+# @return [Hash] The serialized id
 		def to_db
 			return {
 				:key => key,
@@ -142,6 +155,11 @@ class Arml
 		def self.find(arml_id)
 			collection_to_class(arml_id.key).load(arml_id.id)	
 		end
+	
+# Coerces a string to an Id then looks it up
+#
+# @param [String] str
+# @return [Arml::Common]
 		def self.find_s(str)
 			return find(Arml::Id.from_string(str))
 		end
--- lib/arml/base.rb
+++ lib/arml/base.rb
@@ -6,18 +6,22 @@ class Arml
     attr_reader :db
 		alias :old_to_s :to_s
 
+# The db collection name for this instance's class
     def db_key
 			self.class.db_key
     end
 
+# The db collection name for this class
 		def self.db_key
 			const_get('DB_KEY')
 		end
 
+# The actual collection for this class
 		def self.coll
 			Arml.db.collection(db_key)
 		end
 
+# Inherited hook to keep track of classes that have db collections
 		def self.inherited(base)
 			Arml.collection_classes.push(base)
 		end
